3df141e17bcb0c175e57a37ffa93a627f1f73553
99002636 <aditi.dutta@ltts.com>
2021-03-12 17:22:29 +0530

    commit

---
+++ TestHibernate/src/main/java/com/ltts/TestHibernate/App.java
@@ -0,0 +1,26 @@
+package com.ltts.TestHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Configuration cfg=new Configuration();
+       cfg.configure("hibernate.cfg.xml");
+       SessionFactory factory=cfg.buildSessionFactory();
+       Session session=factory.openSession();
+       Transaction t=session.beginTransaction();
+       Employee e= new Employee();
+       e.setEmpid(101);
+       e.setEmpname("sai");
+       e.setEmpaddress("kurnool");
+       session.save(e);
+       t.commit();
+       factory.close();
+       session.close();
+    }
+}
